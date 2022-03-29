8fc4e223cbaa4b1d0782dadc6648e4998c12e05c
Krzysztof Potocki <kpotocki89@gmail.com>
2022-03-29 23:03:13 +0200

    Add app.js, app.initPages(), settings.js

---
+++ src/js/app.js
@@ -0,0 +1,64 @@
+import { settings, select, classNames} from './settings.js';
+
+const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activatePage(id);
+
+        window.location.hash = `#/${id}`;
+      });
+    }
+
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == `#${pageId}`);
+    }
+  },
+
+  init: function(){
+    const thisApp = this;
+    console.log('*** App starting ***');
+    console.log('thisApp:', thisApp);
+    console.log('classNames:', classNames);
+    console.log('settings:', settings);
+    thisApp.initPages();
+  },
+
+};
+
+app.init();
---
+++ src/js/settings.js
@@ -0,0 +1,33 @@
+export const select = {
+  
+  containerOf: {
+    pages: '#pages',
+    about: '.about-wrapper',
+    finder: '.finder-wrapper',
+  },
+  all: {
+    
+  },
+  
+  nav: {
+    links: '.main-nav a',
+  },
+};
+
+export const classNames = {
+  cell: {
+    
+  },
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  }
+};
+
+export const settings = {
+  xxx: {
+   
+  },
+};
